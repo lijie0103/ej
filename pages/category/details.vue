98ba85332762c74aca401f3b441ac6bff2ac31ff
lijie0103 <66655323+lijie0103@users.noreply.github.com>
2020-11-07 14:16:49 +0800

    Add files via upload

---
+++ pages/category/details.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <el-link type="primary" @click='backHandler'>返回</el-link>
+        <p>产品名称：{{this.$route.params.id.name}}</p>
+        <p>产品编号：{{this.$route.params.id.id}}</p>
+        <p>产品序号：{{this.$route.params.id.num}}</p>
+        <p>产品父栏目: {{this.$route.params.id.parentId}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    created() {
+        // console.log(this.$route.params.id);
+    },
+    methods: {
+        //返回上一级
+        backHandler(){
+            this.$router.push({name:'product'})
+        }
+    },
+}
+</script>
